46f03baa548cf3dbe85eb5e88c2f2bbe0179b6dc
Jonathan Wohl <jonathan@sanbornmediafactory.com>
2011-03-09 12:37:45 -0500

    initial commit

---
+++ template.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+Base template used by new-project to know what modules to install where & where to get them from
+Structure is likely to change at some point
+*/
+
+$template = array(
+	'sapphire' => new Github(array(
+		'user' => 'silverstripe', 
+		'project' => 'sapphire', 
+		'branch' => SAPPHIRE_CURRENT_BRANCH
+	)),
+	'cms' => new Github(array(
+		'user' => 'silverstripe', 
+		'project' => 'silverstripe-cms',
+		'branch' => SAPPHIRE_CURRENT_BRANCH
+	)),
+	'themes/html5' => new Github(array(
+		'user' => 'jmwohl', 
+		'project' => 'SilverStripe-HTML5Boilerplate-Theme',
+		'branch' => SAPPHIRE_CURRENT_BRANCH
+	))
+);
+
